e75744f40105f00edf320430b259504704f7c1f9
kiljoy001 <kiljoy001@gmail.com>
2018-02-12 22:11:37 -0800

    chapter 8 init commit

---
+++ chapter8/SportsStore/SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.WebUI.Models
+{
+    public class ProductsListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public PagingInfo Paginginfo { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
